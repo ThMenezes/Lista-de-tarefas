b5c9f7f594bc677791df4f873770af9019225ce1
ThMenezes <thita.menezes24@gmail.com>
2022-11-23 23:42:16 -0300

    adicionando focus no script

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let contador = 0;
 let input = document.getElementById('inputTarefas');
 let button = document.getElementById('btn-adicionar');
 let main = document.getElementById('campoLista');
@@ -5,8 +6,10 @@ let main = document.getElementById('campoLista');
 function adicionarTarefa() {
     let valorInput = input.value;
 
+    ++contador;
+
     if ((valorInput !== "") && (valorInput !== null) && (valorInput !== undefined)){
-        let novaTarefa = `<div class="item">
+        let novaTarefa = `<div id="${contador}" class="item">
         <div class="item-icone">
             <i class="mdi mdi-circle-outline"></i>        
         </div>
@@ -14,10 +17,19 @@ function adicionarTarefa() {
             ${valorInput}
         </div>
         <div class="item-botao">
-            <button class="delete">
+            <button onclick= "deletarTarefa(${contador})" class="delete">
             <i class="mdi mdi-delete"></i> Deletar </button>        
         </div>
-    </div>` 
+    </div>`; 
     main.innerHTML += novaTarefa;
+
+    input.value = "";
+    input.focus();    
+
+    }
+    function deletarTarefa(id){
+    var tarefa = document.getElementById(id)
+        tarefa.remove();
     }
 }
+
