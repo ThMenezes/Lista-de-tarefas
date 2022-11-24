51c972945458a61c1ea9315f4a3a2a24966f08b8
ThMenezes <thita.menezes24@gmail.com>
2022-11-24 11:02:46 -0300

    adicionando função deletarTarefa e tarefaFeita

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 let contador = 0;
 let input = document.getElementById('inputTarefas');
-let button = document.getElementById('btn-adicionar');
+let btnAdicionar = document.getElementById('btn-adicionar');
 let main = document.getElementById('campoLista');
 
 function adicionarTarefa() {
-    let valorInput = input.value;
-
-    ++contador;
+    let valorInput = input.value;    
 
     if ((valorInput !== "") && (valorInput !== null) && (valorInput !== undefined)){
+
+        ++contador;
+
         let novaTarefa = `<div id="${contador}" class="item">
-        <div class="item-icone">
-            <i class="mdi mdi-circle-outline"></i>        
+        <div onclick="tarefaFeita(${contador})" class="item-icone">
+            <i id="icone_${contador}" class="mdi mdi-circle-outline"></i>        
         </div>
-        <div class="item-nome">
+        <div onclick="tarefaFeita(${contador})" class="item-nome">
             ${valorInput}
         </div>
         <div class="item-botao">
-            <button onclick= "deletarTarefa(${contador})" class="delete">
+            <button onclick="deletarTarefa(${contador})" class="delete">
             <i class="mdi mdi-delete"></i> Deletar </button>        
         </div>
     </div>`; 
@@ -27,9 +28,28 @@ function adicionarTarefa() {
     input.focus();    
 
     }
-    function deletarTarefa(id){
-    var tarefa = document.getElementById(id)
-        tarefa.remove();
-    }
 }
 
+function deletarTarefa(id){
+    var tarefa = document.getElementById(id)
+        tarefa.remove();
+}
+
+function tarefaFeita(id) {
+    var item = document.getElementById(id)
+    var classe = item.getAttribute('class');
+
+    if(classe == "item") {
+        item.classList.add('clicado')
+
+        var icone = document.getElementById('icone_' + id)
+        icone.classList.remove("mdi-circle-outline");
+        icone.classList.add("mdi-check-circle");
+    } else {
+        item.classList.remove('clicado')
+
+        var icone = document.getElementById('icone_' + id)
+        icone.classList.remove("mdi-check-circle");
+        icone.classList.add("mdi-circle-outline");
+    }
+}
